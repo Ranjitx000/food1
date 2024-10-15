82a18b8754e9f6881d812ad81635ef233e2c1e5b
Ranjitx000 <133473060+Ranjitx000@users.noreply.github.com>
2024-10-15 11:50:47 +0530

    added index.html, style.css,script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function filterMenu(category) {
+    const allItems = document.querySelectorAll('.menu-item');
+
+    allItems.forEach(item => {
+        if (category === 'all') {
+            item.style.display = 'block';
+        } else if (item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
